e212e742a6911a5c2c698a5fdeb1ad7d88f2d610
kent <kentoyfueconcillo@gmail.com>
2023-08-01 10:07:33 +0800

    fixes user and admin. add gitignore and sms

--- sample.py
+++ sample.py
@@ -2,22 +2,30 @@ from flask import Flask, request, jsonify
 from flask_cors import CORS
 
 import pymysql
+from twilio.rest import Client
+
+#twilio account
+#email: kentoyfueconcillo@gmail.comm
+#pass: @K0angleader1234567890
 
 app = Flask(__name__)
 CORS(app, resources={r"/*": {"origins": ["http://192.168.116.1:3000", "http://192.168.0.160:3000"]}})
 
+#http://localhost/phpmyadmin
+#database
 db_config = {
     'host': 'localhost',
     'user': 'root',
     'database': 'helpdesk'
 }
-
-# http://localhost/phpmyadmin
-# for database
-
-# Create a connection to the database
 connection = pymysql.connect(**db_config)
 
+#twilio
+account_sid = 'AC512d82f1fab10c761596c05accedb537'
+auth_token = 'f7b5bf0e78ec1258e41e8b1171683bb6'
+client = Client(account_sid, auth_token)
+
+############### USERS ###################
 @app.route('/users', methods=['GET'])
 def get_users():
     # Create a cursor
@@ -98,9 +106,9 @@ def search_concerns():
 
     try:
         # Execute the query
-        query = "SELECT * FROM concern WHERE name_reported LIKE %s"
+        query = "SELECT * FROM concern WHERE name_reported LIKE %s AND requested_by_user_id=%s"
         search_value = f"%{user_data['search']}%"  # This will be something like "%12345%"
-        cursor.execute(query, (search_value,))
+        cursor.execute(query, (search_value, user_data['user_id']))
         
         # Fetch all the rows
         rows = cursor.fetchall()
@@ -129,6 +137,76 @@ def search_concerns():
         # Close the cursor
         cursor.close()
 
+@app.route('/report-user', methods=['POST'])
+def report_user():
+    user_data = request.get_json()
+    cursor = connection.cursor()
+
+    try:
+        cursor.execute("INSERT INTO concern (requested_by_user_id, name_reported, reason) VALUES (%s, %s, %s)",
+                   (user_data['requested_by_user_id'], user_data['name_reported'], user_data['reason']))
+        connection.commit()
+
+        return jsonify({'data': 'Successfully registered'})
+
+    except Exception as e:
+        # Handle the exception
+        return jsonify({'error': str(e)}), 500
+
+    finally:
+        # Close the cursor
+        cursor.close()         
+
+############### ADMIN ###################
+
+@app.route('/get-all-concerns', methods=['GET'])
+def get_all_concerns():
+    cursor = connection.cursor()
+
+    try:
+        # Execute the query
+        query = "SELECT * FROM concern"
+        cursor.execute(query,)
+        
+        # Fetch all the rows
+        rows = cursor.fetchall()
+        print('hello', rows)
+
+        # Convert the rows to a list of dictionaries
+        concern = []
+        for row in rows:
+            print(row)
+            user = {
+                'id': row[0],
+                'requested_by_user_id': row[1],
+                'name_reported': row[2],
+                'reason': row[3],
+                'schedule_hearing': row[4],
+            }
+            concern.append(user)
+
+        return jsonify(concern)
+
+    except Exception as e:
+        # Handle the exception
+        return jsonify({'error': str(e)}), 500
+
+    finally:
+        # Close the cursor
+        cursor.close()
+
+@app.route('/send-sms', methods=['POST'])
+def send_sms():
+    user_data = request.get_json()
+    message = client.messages.create(
+        body='Hello, this is a test SMS from Python!',
+        from_='+17622499008',  # Replace with your Twilio phone number
+        to='+63' + user_data['to_phone']    # Replace with the recipient's phone number
+    )
+    return jsonify({'data': 'Message Successfully sent'})
+
+############### AUTHS ###################
+
 @app.route('/register', methods=['POST'])
 def register_user():
     user_data = request.get_json()
@@ -149,17 +227,35 @@ def register_user():
         # Close the cursor
         cursor.close()    
 
-@app.route('/report-user', methods=['POST'])
-def report_user():
+@app.route('/search-admin-concerns', methods=['POST'])
+def search_admin_concerns():
     user_data = request.get_json()
     cursor = connection.cursor()
 
     try:
-        cursor.execute("INSERT INTO concern (requested_by_user_id, name_reported, reason) VALUES (%s, %s, %s)",
-                   (user_data['requested_by_user_id'], user_data['name_reported'], user_data['reason']))
-        connection.commit()
+        # Execute the query
+        query = "SELECT * FROM concern WHERE name_reported LIKE %s"
+        search_value = f"%{user_data['search']}%"  # This will be something like "%12345%"
+        cursor.execute(query, (search_value))
+        
+        # Fetch all the rows
+        rows = cursor.fetchall()
+        print('hello', rows)
 
-        return jsonify({'data': 'Successfully registered'})
+        # Convert the rows to a list of dictionaries
+        concern = []
+        for row in rows:
+            print(row)
+            user = {
+                'id': row[0],
+                'requested_by_user_id': row[1],
+                'name_reported': row[2],
+                'reason': row[3],
+                'schedule_hearing': row[4],
+            }
+            concern.append(user)
+
+        return jsonify(concern)
 
     except Exception as e:
         # Handle the exception
@@ -167,7 +263,7 @@ def report_user():
 
     finally:
         # Close the cursor
-        cursor.close()         
+        cursor.close()
 
 @app.route('/login', methods=['POST'])
 def login():
